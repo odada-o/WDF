3570f70c44ddd4276be9b42c8a3795ab49c3fedc
odada <eehd80yul@gmail.com>
2023-09-14 11:54:41 +0900

    1.date

--- 07-javascript/1-js데이터.js
+++ 07-javascript/1-js데이터.js
@@ -45,8 +45,121 @@ age = 20;
 console.log(age);
 
 // undefined
-let name;
-console.log(name);
+let height;
+console.log(height);
 
-name = 'odada';
-console.log(name);
+height = 'odada';
+console.log(height);
+
+// ex
+const user1 = {
+  name: 'odada',
+  age: 20,
+  height: 180,
+  email: null,
+};
+
+console.log(user1.name, user1.age, user1.height, user1);
+console.log(user1.email); // null
+console.log(user1.weight); // undefined
+
+// array (배열)
+const animals = ['dog', 'cat', 'fish'];
+
+console.log(animals);
+console.log(animals[0]); // dog
+console.log(animals[2]); // fish
+console.log(animals.length); // 3
+console.log(animals[animals.length - 1]); // fish
+
+// object (객체)
+const user2 = {
+  name: 'odada',
+  age: 20,
+};
+
+console.log(user2);
+console.log(user2.name); // odada
+console.log(user2.age); // 20
+console.log(user2['name']); // odada
+console.log(user2['age']); // 20
+
+// ex
+const userA = {
+  name: 'kim',
+  age: 50,
+};
+
+const userB = {
+  name: 'lee',
+  age: 20,
+  parent: userA,
+};
+
+console.log(userB);
+console.log(userB.parent.name); // kim
+console.log(userB['parent']['name']); // kim
+
+// 배열에 객체 넣기
+const users = [userA, userB];
+
+console.log(users[0]); // { name: 'kim', age: 50 }
+console.log(users[0].name); // kim
+console.log(users[0]['name']); // kim
+
+// function (함수)
+// 함수 선언식 (기명 함수)
+function printHello() {
+  console.log('hello');
+}
+
+printHello();
+
+console.log(printHello); // [Function: printHello]
+console.log(printHello()); // hello
+
+// 함수 표현식 (익명 함수)
+const getNumber = function () {
+  return 123;
+};
+
+console.log(getNumber); // [Function: getNumber]
+console.log(typeof getNumber); // function
+console.log(getNumber()); // 123
+console.log(typeof getNumber()); // number
+
+// TYPE COVERSTION (타입 변환)
+
+const aa = 1;
+const bb = '1';
+
+console.log(aa == bb); // true
+console.log(aa === bb); // false
+
+const cc = false;
+const dd = 0;
+
+console.log(cc == dd); // true
+console.log(cc === dd); // false
+
+const ee = true;
+const ff = 1;
+
+console.log(ee == ff); // true
+console.log(ee === ff); // false
+
+// truthy (참 같은 값) & falsy (거짓 같은 값)
+
+if (true) {
+  console.log('true');
+}
+
+// 데이터 타입 확인 (typeof)
+console.log(typeof '123');
+console.log(typeof 123);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function () {});
